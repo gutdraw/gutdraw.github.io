b9647b74c0b468aa06db8b8416fb67193344e103
gutdraw <sato.32@osu.edu>
2023-01-24 17:51:11 -0800

    quacks game
    quacks

---
+++ quacksgame/duck/duck.js
@@ -0,0 +1,28 @@
+/* eslint-disable require-yield, eqeqeq */
+
+import {
+  Sprite,
+  Trigger,
+  Watcher,
+  Costume,
+  Color,
+  Sound
+} from "https://unpkg.com/leopard@^1/dist/index.esm.js";
+
+export default class _96967876PictureLibraryDownloadDuckSvgPngIconFree extends Sprite {
+  constructor(...args) {
+    super(...args);
+
+    this.costumes = [
+      new Costume(
+        "96-967876_picture-library-download-duck-svg-png-icon-free",
+        "./duck/costumes/duck.png",
+        { x: 365.5, y: 360 }
+      )
+    ];
+
+    this.sounds = [];
+
+    this.triggers = [];
+  }
+}
